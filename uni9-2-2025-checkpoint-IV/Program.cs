fda5793953cc390402c580fc4a1e06efbb679aad
isabellasena13 <isabellasena05@gmail.com>
2025-11-17 23:33:39 +0000

    checkpoint IV

---
+++ uni9-2-2025-checkpoint-IV/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public class Program
+{
+ public static void Main()
+'' {
+                
+double salarioBruto = 3000.00;
+ double inss = 258.83;
+CalculadoraIRRF calculadora = new CalculadoraIRRF();
+
+double irrf = calculadora.Calcular(salarioBruto, inss);
+
+Console.WriteLine("IRRF: " + irrf.ToString("F2"));
+    }
+}
